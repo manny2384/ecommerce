2d7fac31749d5ba4db62354054d21cda8bb061ff
manny2384 <gonzalezeh2015@gmail.com>
2022-06-04 17:36:21 -0700

    added some home page features

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import './App.css';
 import logo from './assets/shared/desktop/logo.svg';
 import cart from './assets/shared/desktop/icon-cart.svg';
 
+// import components
+import Footer from './components/footer';
+
 // images imported
 import hero from './assets/home/desktop/image-hero.jpg';
 import hero_tablet from './assets/home/tablet/image-header.jpg';
 import hero_mobile from './assets/home/mobile/image-header.jpg';
+
+// icons
+import arrowRight from './assets/shared/desktop/icon-arrow-right.svg';
+
+// featured products
+import headphones from './assets/shared/desktop/image-category-thumbnail-headphones.png';
+import earphones from './assets/shared/desktop/image-category-thumbnail-earphones.png';
+import speakers from './assets/shared/desktop/image-category-thumbnail-speakers.png';
+
 
 function App() {
 
@@ -43,11 +55,6 @@ function App() {
         </nav>
 
         <div className='header-content'>
-          <picture>
-            <source srcset={hero_tablet} media="(min-width: 600px)" />
-            <source srcset={hero} media="(min-width: 1000px)" />
-            <img src={hero_mobile} alt="headphones" />
-          </picture>
 
           <div className='header-product'>
             <span className='item'>NEW PRODUCT</span>
@@ -58,13 +65,58 @@ function App() {
             <span className='item see-prod-btn'>See product</span>
    
           </div>
+          
+          <picture>
+            <source srcset={hero} media="(min-width: 1000px)" />
+            <source srcset={hero_tablet} media="(min-width: 600px)" />
+            <img src={hero_mobile} alt="headphones" />
+          </picture>
+
         </div>
       </header>
-
       
+      <div className='featured-products'>
+        <div className='item'>
+          <span>
+            <img src={headphones} alt="headphones" />
+          </span>
+          <span> Headphones </span>
+          <span>
+            Shop <img src={arrowRight} alt="arrowright" />
+          </span>
+          <span></span>
+        </div>
+        <div className='item'>
+          <span>
+            <img src={speakers} alt="speakers" />
+          </span>
+          <span> Speakers </span>
+          <span>
+            Shop <img src={arrowRight} alt="arrowright" />
+          </span>
+          <span></span>
+        </div>
+        <div className='item'>
+          <span>
+            <img src={earphones} alt="earphones" />
+          </span>
+          <span> Earphones </span>
+          <span>
+            Shop <img src={arrowRight} alt="arrowright" />
+          </span>
+          <span></span>
+        </div>
 
 
-          
+
+      </div>
+      
+      <div className='placeholder'>
+
+      </div>
+
+
+      <Footer logo={logo} />
 
     </div>);
 }
---
+++ src/components/footer.js
@@ -0,0 +1,37 @@
+// social media icons
+import twitter from '../assets/shared/desktop/icon-twitter.svg';
+import facebook from '../assets/shared/desktop/icon-facebook.svg';
+import instagram from '../assets/shared/desktop/icon-instagram.svg';
+
+function Footer(props){
+    return(<div className="footer">
+        <div className="footer-branding">
+            <img src={props.logo} alt="brand" />
+
+            <div className="footer-nav">
+                <ul>
+                    <li> HOME </li>
+                    <li> HEADPHONES </li>
+                    <li> SPEAKERS </li>
+                    <li> EARPHONES </li>
+                </ul>
+            </div>
+        </div>
+
+        <div className="footer-desc">
+        Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers 
+        and sound specialists who are devoted to helping you get the most out of personal audio. Come and 
+        visit our demo facility - we’re open 7 days a week.
+        </div>
+        <div className='footer-tags'>
+        <div className="copyrights"> Copyright 2021. All Rights Reserved </div>
+        <div className="socialMedia">
+            <img src={facebook} alt="facebookicon" />
+            <img src={twitter} alt="twittericon" />
+            <img src={instagram} alt="instagramicon" />
+        </div>
+        </div>
+    </div>)
+}
+
+export default Footer;
