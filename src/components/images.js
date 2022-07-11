a05c0020bbb983bf8c2afabc94b0a60e6a14cffa
manny2384 <gonzalezeh2015@gmail.com>
2022-07-11 10:18:37 -0700

    added gallery images

--- src/components/images.js
+++ src/components/images.js
@@ -1,4 +1,5 @@
 const Images = {
+    // main product images
     "XX99 MARK II HEADPHONES desktop": require('../assets/product-xx99-mark-two-headphones/desktop/image-product.jpg'),
     "XX99 MARK II HEADPHONES tablet": require('../assets/product-xx99-mark-two-headphones/tablet/image-product.jpg'),
     "XX99 MARK II HEADPHONES mobile": require('../assets/product-xx99-mark-two-headphones/mobile/image-product.jpg'),
@@ -23,6 +24,66 @@ const Images = {
     "YX1 WIRELESS EARPHONES tablet": require('../assets/product-yx1-earphones/tablet/image-product.jpg'),
     "YX1 WIRELESS EARPHONES mobile": require('../assets/product-yx1-earphones/mobile/image-product.jpg'),
 
+    // gallery images
+    "XX99 MARK II HEADPHONES 1 desktop": require("../assets/product-xx99-mark-one-headphones/desktop/image-gallery-1.jpg"),
+    "XX99 MARK II HEADPHONES 1 tablet": require("../assets/product-xx99-mark-one-headphones/tablet/image-gallery-1.jpg"),
+    "XX99 MARK II HEADPHONES 1 mobile": require("../assets/product-xx99-mark-one-headphones/mobile/image-gallery-1.jpg"),
+    "XX99 MARK II HEADPHONES 2 desktop": require("../assets/product-xx99-mark-one-headphones/desktop/image-gallery-2.jpg"),
+    "XX99 MARK II HEADPHONES 2 tablet": require("../assets/product-xx99-mark-one-headphones/tablet/image-gallery-2.jpg"),
+    "XX99 MARK II HEADPHONES 2 mobile": require("../assets/product-xx99-mark-one-headphones/mobile/image-gallery-2.jpg"),
+    "XX99 MARK II HEADPHONES 3 desktop": require("../assets/product-xx99-mark-one-headphones/desktop/image-gallery-3.jpg"),
+    "XX99 MARK II HEADPHONES 3 tablet": require("../assets/product-xx99-mark-one-headphones/tablet/image-gallery-3.jpg"),
+    "XX99 MARK II HEADPHONES 3 mobile": require("../assets/product-xx99-mark-one-headphones/mobile/image-gallery-3.jpg"),
+
+    "XX99 MARK I HEADPHONES 1 desktop": require("../assets/product-xx99-mark-two-headphones/desktop/image-gallery-1.jpg"),
+    "XX99 MARK I HEADPHONES 1 tablet": require("../assets/product-xx99-mark-two-headphones/tablet/image-gallery-1.jpg"),
+    "XX99 MARK I HEADPHONES 1 mobile": require("../assets/product-xx99-mark-two-headphones/mobile/image-gallery-1.jpg"),
+    "XX99 MARK I HEADPHONES 2 desktop": require("../assets/product-xx99-mark-two-headphones/desktop/image-gallery-2.jpg"),
+    "XX99 MARK I HEADPHONES 2 tablet": require("../assets/product-xx99-mark-two-headphones/tablet/image-gallery-2.jpg"),
+    "XX99 MARK I HEADPHONES 2 mobile": require("../assets/product-xx99-mark-two-headphones/mobile/image-gallery-2.jpg"),
+    "XX99 MARK I HEADPHONES 3 desktop": require("../assets/product-xx99-mark-two-headphones/desktop/image-gallery-3.jpg"),
+    "XX99 MARK I HEADPHONES 3 tablet": require("../assets/product-xx99-mark-two-headphones/tablet/image-gallery-3.jpg"),
+    "XX99 MARK I HEADPHONES 3 mobile": require("../assets/product-xx99-mark-two-headphones/mobile/image-gallery-3.jpg"),
+
+    "XX59 HEADPHONES 1 desktop": require("../assets/product-xx59-headphones/desktop/image-gallery-1.jpg"),
+    "XX59 HEADPHONES 1 tablet": require("../assets/product-xx59-headphones/tablet/image-gallery-1.jpg"),
+    "XX59 HEADPHONES 1 mobile": require("../assets/product-xx59-headphones/mobile/image-gallery-1.jpg"),
+    "XX59 HEADPHONES 2 desktop": require("../assets/product-xx59-headphones/desktop/image-gallery-2.jpg"),
+    "XX59 HEADPHONES 2 tablet": require("../assets/product-xx59-headphones/tablet/image-gallery-2.jpg"),
+    "XX59 HEADPHONES 2 mobile": require("../assets/product-xx59-headphones/mobile/image-gallery-2.jpg"),
+    "XX59 HEADPHONES 3 desktop": require("../assets/product-xx59-headphones/desktop/image-gallery-3.jpg"),
+    "XX59 HEADPHONES 3 tablet": require("../assets/product-xx59-headphones/tablet/image-gallery-3.jpg"),
+    "XX59 HEADPHONES 3 mobile": require("../assets/product-xx59-headphones/mobile/image-gallery-3.jpg"),
+    
+    "ZX9 SPEAKER 1 desktop": require("../assets/product-zx9-speaker/desktop/image-gallery-1.jpg"),
+    "ZX9 SPEAKER 1 tablet": require("../assets/product-zx9-speaker/tablet/image-gallery-1.jpg"),
+    "ZX9 SPEAKER 1 mobile": require("../assets/product-zx7-speaker/mobile/image-gallery-1.jpg"),
+    "ZX9 SPEAKER 2 desktop": require("../assets/product-zx9-speaker/desktop/image-gallery-2.jpg"),
+    "ZX9 SPEAKER 2 tablet": require("../assets/product-zx9-speaker/tablet/image-gallery-2.jpg"),
+    "ZX9 SPEAKER 2 mobile": require("../assets/product-zx9-speaker/mobile/image-gallery-2.jpg"),
+    "ZX9 SPEAKER 3 desktop": require("../assets/product-zx9-speaker/desktop/image-gallery-3.jpg"),
+    "ZX9 SPEAKER 3 tablet": require("../assets/product-zx9-speaker/tablet/image-gallery-3.jpg"),
+    "ZX9 SPEAKER 3 mobile": require("../assets/product-zx9-speaker/mobile/image-gallery-3.jpg"),
+
+    "ZX7 SPEAKER 1 desktop": require("../assets/product-zx7-speaker/desktop/image-gallery-1.jpg"),
+    "ZX7 SPEAKER 1 tablet": require("../assets/product-zx7-speaker/tablet/image-gallery-1.jpg"),
+    "ZX7 SPEAKER 1 mobile": require("../assets/product-zx7-speaker/mobile/image-gallery-1.jpg"),
+    "ZX7 SPEAKER 2 desktop": require("../assets/product-zx7-speaker/desktop/image-gallery-2.jpg"),
+    "ZX7 SPEAKER 2 tablet": require("../assets/product-zx7-speaker/tablet/image-gallery-2.jpg"),
+    "ZX7 SPEAKER 2 mobile": require("../assets/product-zx7-speaker/mobile/image-gallery-2.jpg"),
+    "ZX7 SPEAKER 3 desktop": require("../assets/product-zx7-speaker/desktop/image-gallery-3.jpg"),
+    "ZX7 SPEAKER 3 tablet": require("../assets/product-zx7-speaker/tablet/image-gallery-3.jpg"),
+    "ZX7 SPEAKER 3 mobile": require("../assets/product-zx7-speaker/mobile/image-gallery-3.jpg"),
+
+    "YX1 WIRELESS EARPHONES 1 desktop": require("../assets/product-yx1-earphones/desktop/image-gallery-1.jpg"),
+    "YX1 WIRELESS EARPHONES 1 tablet": require("../assets/product-yx1-earphones/tablet/image-gallery-1.jpg"),
+    "YX1 WIRELESS EARPHONES 1 mobile": require("../assets/product-yx1-earphones/mobile/image-gallery-1.jpg"),
+    "YX1 WIRELESS EARPHONES 2 desktop": require("../assets/product-yx1-earphones/desktop/image-gallery-2.jpg"),
+    "YX1 WIRELESS EARPHONES 2 tablet": require("../assets/product-yx1-earphones/tablet/image-gallery-2.jpg"),
+    "YX1 WIRELESS EARPHONES 2 mobile": require("../assets/product-yx1-earphones/mobile/image-gallery-2.jpg"),
+    "YX1 WIRELESS EARPHONES 3 desktop": require("../assets/product-yx1-earphones/desktop/image-gallery-3.jpg"),
+    "YX1 WIRELESS EARPHONES 3 tablet": require("../assets/product-yx1-earphones/tablet/image-gallery-3.jpg"),
+    "YX1 WIRELESS EARPHONES 3 mobile": require("../assets/product-yx1-earphones/mobile/image-gallery-3.jpg"),
 }
 
 export default Images;
--- src/components/product.js
+++ src/components/product.js
@@ -12,12 +12,14 @@ function Product(props){
     }, [quantity]);
 
     const[box] = useState(myData[props.product]['in_the_box']);
+    const[features] = useState(myData[props.product]['features']);
     const[q] = useState(myData[props.product]['in_the_box_q']);
 
 
     return(<div className='product-element'>
 
         <div className='main-info'>
+
             <picture> 
                 <source srcSet={ IMAGES[props.product + " desktop"] } media='(min-width: 1000px)' />
                 <source srcSet={ IMAGES[props.product + " tablet"] } media='(min-width: 700px)' />
@@ -25,34 +27,42 @@ function Product(props){
             </picture>
 
             <div className='main-info-desc'>
-                {myData[props.product]['new'] && <span> NEW PRODUCT </span>}
-                <span> {props.product} </span>
-                <span> {myData[props.product]['description']} </span>
-                <span> $ {myData[props.product]['price']} </span>
-                <span> 
+                {
+                    myData[props.product]['new'] && 
+                    <span style={{'color':'#D87D4A'}}> NEW PRODUCT </span>
+                }
+                <span style={{'font-size': '28px', 'letterSpacing':'1px'}}> {props.product} </span>
+                <span style={{'font-size': '15px', 'opacity':'0.5'}}> {myData[props.product]['description']} </span>
+                <span style={{'font-size': '18px'}}> $ {myData[props.product]['price']} </span>
+                <span className='counter'> 
                     <div className="quantity-counter"> 
                         <span onClick={()=>{
                             if(quantity > 0)
                                 setQuantity(quantity - 1);
                         }}> - </span> 
-
                         <input type="number" name="quantity" value={quantity} />
-
                         <span onClick={()=>{setQuantity(quantity + 1)}}> + </span>
                     </div>
                     
                     <button> ADD TO CART </button>
                 </span>
             </div>
+
         </div>
 
         <div>
+
             <div className='features'>
-                <p> {myData[props.product]["features"][0]} </p>
-                <p> {myData[props.product]["features"][1]} </p>
+                <div> FEATURES </div>
+                <ul>
+                    {features.map((elem, i)=>{
+                        return(<li>{elem}</li>)
+                    })}
+                </ul>
+
             </div>
             <div className='in-the-box'>
-                <div> IN THE BOX </div>
+                <div style={{'font-size':'24px'}}> IN THE BOX </div>
                 <ul>
                     {box.map((elem, i)=>{
                         return(<li> <span> {q[i]}x </span> <span> {elem} </span> </li>)
@@ -62,24 +72,40 @@ function Product(props){
 
         </div>
 
-        <div className='product-imgs'>
+        <div className='product-gallery'>
+
             <div> 
-                <picture> </picture>
-                <picture> </picture>
+                <picture> 
+                    <source srcSet={IMAGES[props.product + " 1 desktop"]} media="(min-width: 1000px)"/>
+                    <source srcSet={IMAGES[props.product + " 1 tablet"]} media="(min-width: 700px)"/>
+                    <img src={IMAGES[props.product + " 1 mobile"]} alt="image_gallery_1"/>
+                </picture>
+                <picture> 
+                <source srcSet={IMAGES[props.product + " 2 desktop"]} media="(min-width: 1000px)"/>
+                    <source srcSet={IMAGES[props.product + " 2 tablet"]} media="(min-width: 700px)"/>
+                    <img src={IMAGES[props.product + " 2 mobile"]} alt="image_gallery_2"/>
+                </picture>
             </div>
 
             <div> 
-                <picture> </picture>
+                <picture>
+                <source srcSet={IMAGES[props.product + " 3 desktop"]} media="(min-width: 1000px)"/>
+                    <source srcSet={IMAGES[props.product + " 3 tablet"]} media="(min-width: 700px)"/>
+                    <img src={IMAGES[props.product + " 3 mobile"]} alt="image_gallery_3"/>
+                </picture>
             </div>
+
         </div>
 
-        <div className='extra-products'>
+        <div className='promotion-gallery'>
+
             <div> YOU MAY ALSO LIKE </div>
             <div>
                 <div> </div>
                 <div> </div>
                 <div> </div>
             </div>
+            
         </div>
 
     </div>);
